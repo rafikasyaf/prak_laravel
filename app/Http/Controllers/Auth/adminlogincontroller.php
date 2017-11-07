b74b3e1c6e70b0cf754f20f943a9554079db77cf
rafikasyaf <rafika1504@gmail.com>
2017-11-07 08:54:35 +0700

    admin login

--- app/Http/Controllers/Auth/adminlogincontroller.php
+++ app/Http/Controllers/Auth/adminlogincontroller.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Auth;
 
-class adminlogincontroller extends Controller
+class AdminLoginController extends Controller
 {
 	public function __construct()
 	{
@@ -24,9 +24,8 @@ class adminlogincontroller extends Controller
 		'password' => 'required|min:6',
 		]);
 		// Attempt to log the user in
-		if (Auth::guard('admin')->attempt(['email' => $request->email, 'password' => $request->password],
-		$request->remember)) {
-		// If successful, then redirect to their intended location
+		if (Auth::guard('admin')->attempt(['email' => $request->email, 'password' => $request->password], $request->remember)) {
+			// If successful, then redirect to their intended location
 			return redirect()->intended(route('admin.dashboard'));
 		}
 		// If unsuccessful, then redirect back to the login with the form data
